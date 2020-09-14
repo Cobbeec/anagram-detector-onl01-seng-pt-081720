f43ee26b90bd243f928c7df74cc235f13e926e85
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-14 21:57:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram 
+ attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(string)
+    string.each do |w|
+      w.split.sort 
+   
+end 
+
+end 
